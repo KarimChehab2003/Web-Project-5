cb4523f7814ff05a83a0fe96cdcdfe550e857b4b
Karim <karimchehab2018@gmail.com>
2024-02-25 01:42:20 +0200

    test6

--- src/components/GetInTouchComponent.js
+++ src/components/GetInTouchComponent.js
@@ -13,32 +13,14 @@ const GetInTouchComponent = () => {
                 </Col>
                 <Col className='d-flex flex-column flex-wrap py-2'>
                     <p className="title-right text-center">Get In Touch</p>
-                    <Container className="form-right" style={{ width: "70%" }}>
-                        <Row className="">
-                            <Col className="d-flex my-2">
-                                <input type="text" placeholder="First Name" className="input-right flex-grow-1" />
-                            </Col>
-                            <Col className="d-flex my-2">
-                                <input type="text" placeholder="Last Name" className="input-right flex-grow-1" />
-                            </Col>
-                        </Row>
-                        <Row className="">
-                            <Col className="d-flex my-2">
-                                <input type="text" placeholder="Email Address" className="input-right flex-grow-1" />
-                            </Col>
-                            <Col className="d-flex my-2">
-                                <input type="text" placeholder="Phone No." className="input-right flex-grow-1" />
-                            </Col>
-                        </Row>
-                        <Row className="">
-                            <Col className="d-flex my-2">
-                                <textarea className="textarea-right flex-grow-1" rows="5" placeholder="Message" />
-                            </Col>
-                        </Row>
-                    </Container>
-                    <Container className="d-flex" style={{ width: "70%" }}>
-                        <input type="submit" className="send-right " value="Send" />
-                    </Container>
+                    <form className="form-right">
+                        <input type="text" placeholder="First Name" className="input-right" />
+                        <input type="text" placeholder="Last Name" className="input-right" />
+                        <input type="text" placeholder="Email Address" className="input-right" />
+                        <input type="text" placeholder="Phone No." className="input-right" />
+                        <textarea className="textarea-right" rows="5" placeholder="Message" />
+                    </form>
+                        <input type="submit" className="send-right mx-2" value="Send" />
                 </Col>
             </Row>
 
@@ -48,8 +30,8 @@ const GetInTouchComponent = () => {
                         <p className="text-capitalize fs-3 text-start text-break" >See my projects at once & leave here your e-mail address</p>
                     </Col>
                     <Col className="d-flex align-items-center">
-                        <Container className="leave-email-input d-flex justify-content-between pe-0 " style={{width: "80%"}}>
-                            <input type="email" className="email-input" placeholder="Email Address" style={{width: "60%"}}/>
+                        <Container className="leave-email-input d-flex justify-content-between pe-0 " style={{ width: "80%" }}>
+                            <input type="email" className="email-input" placeholder="Email Address" style={{ width: "60%" }} />
                             <input type="submit" className="email-submit-button py-2" value="Submit" />
                         </Container>
                     </Col>
